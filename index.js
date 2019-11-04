3dc30d056f69de662e2146b5e42543f1edb60e7a
cmulliss <cmulliss@gmail.com>
2019-11-04 15:55:07 +0000

    started project

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = <div>React element with jsx</div>
+
+ReactDOM.render(element, document.getElementById('root'))
+
+// const element = React.createElement('div', null, 'React element!')
